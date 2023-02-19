058fc4e42344e3bf44ba7b3f882220cafb52492e
mirindi19 <mirindisaidi19@gmail.com>
2023-02-19 02:30:39 -0800

    railway

--- SERVER/src/configs/db.js
+++ SERVER/src/configs/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 //LOAD DOTENV FILES
-dotenv.config({ path: "./src/configs/config.env" });
-
+// dotenv.config({ path: "./src/configs/config.env" });
+dotenv.config();
+mongoose.set('strictQuery', true);
 const connectionString = process.env.MONGOOSE_URI;
 const dbConnect = async (next) => {
     mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
